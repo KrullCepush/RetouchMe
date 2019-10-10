01ab4495e526d80de1afd1de33e64b7999095096
Mikhail Khusnudinov <mikh.khu@gmail.com>
2019-10-10 14:54:03 +0300

    re middleware/auth

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const notifRouter = require('./routes/notif');
+// const notifRouter = require('./routes/notif');
 
 const app = express();
+
+app.use(
+  session({
+    store: new MongoStore({
+      url: 'mongodb://localhost:27017/retouchme',
+      stringify: false,
+    }),
+    cookie: {
+      maxAge: 24 * 360000,
+    },
+    secret: 'aws-01-d',
+    resave: true,
+    saveUninitialized: false,
+  }),
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/notif', notifRouter);
+// app.use('/notif', notifRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -18,8 +18,26 @@ const getUserId = (req, res, next) => {
   return req.user && req.user._id;
 };
 
+const isAdmin = (req, res, next) => {
+  if (req.session.user.admin && req.cookies.user_sid) {
+    next();
+  } else {
+    res.redirect('/dashboard');
+  }
+};
+
+const isAuthorized = (req, res, next) => {
+  if (req.session.user.admin || req.session.user._id === req.params.id) {
+    next();
+  } else {
+    res.redirect('/dashboard');
+  }
+};
+
 module.exports = {
   sessionChecker,
   noSessionChecker,
   getUserId,
+  isAdmin,
+  isAuthorized,
 };
